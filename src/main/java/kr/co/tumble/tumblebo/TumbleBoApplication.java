1038966afb21cfad50d465588929dce42d0c37fa
palaoh <palaoh@gmail.com>
2023-07-31 10:40:02 +0900

    start

--- src/main/java/kr/co/tumble/tumblebo/TumbleBoApplication.java
+++ src/main/java/kr/co/tumble/tumblebo/TumbleBoApplication.java
@@ -2,8 +2,9 @@ package kr.co.tumble.tumblebo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
 public class TumbleBoApplication {
 
 	public static void main(String[] args) {
