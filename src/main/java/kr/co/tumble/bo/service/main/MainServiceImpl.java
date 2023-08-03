70bfded7f1bfd1782a5f314a01cde7495591125e
palaoh <palaoh@plateer.com>
2023-08-03 13:01:14 +0900

    20230803

---
+++ src/main/java/kr/co/tumble/bo/service/main/MainServiceImpl.java
@@ -0,0 +1,24 @@
+package kr.co.tumble.bo.service.main;
+
+import kr.co.tumble.common.rest.RestApiUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName    : kr.co.tumble.bo.service.main
+ * fileName       : MainServiceImpl
+ * author         : DANIEL
+ * date           : 2023-08-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-08-01        DANIEL       최초 생성
+ */
+
+@Service
+@RequiredArgsConstructor
+public class MainServiceImpl {
+
+    private final RestApiUtil restApiUtil;
+}
